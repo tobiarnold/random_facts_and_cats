7691e08dcfb508c19770e094333103ee8ecd2832
tobiarnold <77453801+tobiarnold@users.noreply.github.com>
2023-02-03 17:04:50 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict";
+function get_data() {
+   Promise.all([
+      fetch("https://uselessfacts.jsph.pl/random.json?language=de").then(res => res.json()),
+      fetch("https://aws.random.cat/meow").then(res => res.json())
+    ])
+      .then(data => {
+        document.querySelector("#text").innerHTML = data[0].text;
+        document.querySelector("#cat-image").src = data[1].file;
+      })
+      .catch(error => console.error(error));
+  }
