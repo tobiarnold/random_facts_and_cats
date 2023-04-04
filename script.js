0324f6981c229122e88ae3ce219e7afdd94f8deb
tobiarnold <77453801+tobiarnold@users.noreply.github.com>
2023-04-04 19:04:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function get_data() {
     ])
       .then(data => {
         document.querySelector("#text").innerHTML = data[0].text;
-        document.querySelector("#cat-image").src = data[1].url;
+        document.querySelector("#cat-image").src = `https://cataas.com${data[1].url}`;
       })
       .catch(error => console.error(error));
   }
