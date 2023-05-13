2ff45458a5e9c7c84f990677db6f83106f4c200f
tobiarnold <77453801+tobiarnold@users.noreply.github.com>
2023-05-13 22:57:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 function get_data() {
    Promise.all([
       fetch("https://uselessfacts.jsph.pl/random.json?language=de").then(res => res.json()),
-      //fetch("https://cataas.com/cat?json=true").then(res => res.json())
-      fetch("https://randombig.cat/roar.json").then(res => res.json())
+      fetch("https://cataas.com/cat?json=true").then(res => res.json())
+      //fetch("https://randombig.cat/roar.json").then(res => res.json())
     ])
       .then(data => {
         document.querySelector("#text").innerHTML = data[0].text;
